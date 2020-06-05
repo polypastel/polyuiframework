994e4bb621cdb6c92080be8bc34008ebcafd0035
polypastel <66486378+polypastel@users.noreply.github.com>
2020-06-05 10:42:52 -0600

    Create CustomData.lua

---
+++ CustomData.lua
@@ -0,0 +1,12 @@
+--[[
+	polypastel
+	
+	Custom data for individual elements of the UI.
+--]]
+
+local Data = {
+	["AlertButton"] = {OnClick = {"Toggle", "Alert"}, AnimStyle = "Tween"},
+	["Exit"] = {OnClick = {"Close"}, AnimStyle = "Darken"}
+}
+
+return Data
